4418195241838d2a76dffd729365e9ed27fdd05b
Nan Wu <32279235+virus0071@users.noreply.github.com>
2018-07-19 18:58:39 -0400

    Update customer.js

--- customer.js
+++ customer.js
@@ -4,7 +4,7 @@ var Table = require('cli-table');
 
 
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: "127.0.0.1",
     port: 3306,
     user: "root", 
     password: " ", 
